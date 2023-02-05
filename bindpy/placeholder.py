f543acefc2ba8c1da8419fecf68b5e8ecf3605c6
Daniil Dudkin <39276703+unterumarmung@users.noreply.github.com>
2023-02-05 19:31:03 +0000

    Add lint CI

--- bindpy/placeholder.py
+++ bindpy/placeholder.py
@@ -1,3 +1,7 @@
 class Placeholder:
+    """
+    A class that represents placeholders in `bind` function
+    """
+
     def __init__(self, index):
         self.index = index
--- examples/bind_front.py
+++ examples/bind_front.py
@@ -4,10 +4,11 @@ from bindpy import *
 def add(a, b, c=0):
     return a + b + c
 
+
 add_10 = bind_front(add, 10)
 result = add_10(20, c=30)
-print(result) # 60
+print(result)  # 60
 
 add_20_30 = bind_front(add, 20, 30)
 result = add_20_30()
-print(result) # 50
+print(result)  # 50
